246b1f95b8a1aa411d462177936d01c5cfdf55f0
Марина Коновалова <konovalova.owl@gmail.com>
2022-01-27 21:57:58 +0500

    init commit

---
+++ main1.py
@@ -0,0 +1,31 @@
+import telebot
+
+bot = telebot.TeleBot("5126809018:AAE_NN_JWcn70WGPhSbKC142ajQbyM4lmzI")
+
+URL = ''
+
+
+def validate_url(message):
+    return True
+
+
+@bot.message_handler(content_types=['photo'])
+def handle_photo(message):
+    print(f'message.photo = {message.photo}')
+    fileID = message.photo[-1].file_id
+    print(f'fileID = {fileID}')
+    file = bot.get_file(fileID)
+    print(f'file.file_path = {file.file_path}')
+
+    bot.send_message(message.chat.id, message)
+
+
+@bot.message_handler(func=validate_url, content_types=['text'])
+def handle_text(message):
+    URL = message.text
+    bot.reply_to(message, URL)
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
+
